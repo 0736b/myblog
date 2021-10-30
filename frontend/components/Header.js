dbffc154006a1657afb5b605b3e5ff9d3d794890
Thanawat <thanawat_bumpengpun@cmu.ac.th>
2021-10-30 17:17:02 +0700

    fix: navbar

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,73 +1,56 @@
-import React from 'react';
-import { useState } from 'react';
-import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink, UncontrolledDropdown, NavbarText, DropdownToggle, DropdownItem, DropdownMenu  } from 'reactstrap';
+import React from "react";
+import { useState } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  NavbarText,
+  DropdownToggle,
+  DropdownItem,
+  DropdownMenu,
+} from "reactstrap";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggle = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div>
-  <Navbar
-    color="dark"
-    container="md"
-    dark
-    expand="md"
-    fixed="top"
-    full
-  >
-    <NavbarBrand href="/">
-      0736b
-    </NavbarBrand>
-    <NavbarToggler onClick={toggle} />
-    <Collapse isOpen={isOpen} navbar>
-      <Nav
-        className="me-auto"
-        navbar
-      >
-        <NavItem>
-          <NavLink href="/components/">
-            Blogs
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="https://github.com/reactstrap/reactstrap">
-            Portfolio
-          </NavLink>
-        </NavItem>
-        <UncontrolledDropdown
-          inNavbar
-          nav
-        >
-          <DropdownToggle
-            caret
-            nav
-          >
-            Socials
-          </DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem>
-              Github
-            </DropdownItem>
-            <DropdownItem>
-              LinkedIn
-            </DropdownItem>
-            <DropdownItem>
-              Dev.to
-            </DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-      </Nav>
-      <NavbarText>
-        Hello world!
-      </NavbarText>
-    </Collapse>
-  </Navbar>
-</div>
+      <Navbar color="dark" container="md" dark expand="md" fixed="top">
+        <NavbarBrand href="/">0736b</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/0736b">
+                Portfolio
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown inNavbar nav>
+              <DropdownToggle caret nav>
+                Socials
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Github</DropdownItem>
+                <DropdownItem>LinkedIn</DropdownItem>
+                <DropdownItem>Dev.to</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
   );
 };
 
