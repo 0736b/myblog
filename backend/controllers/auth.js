510a88c33f98503cfe627ba861f84f4ff8850d1a
Thanawat <thanawat_bumpengpun@cmu.ac.th>
2021-11-01 13:48:22 +0700

    add: admin protected route

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,6 +3,7 @@ const shortId = require('shortid');
 const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
 
+
 exports.signup = (req, res) => {
 
     User.findOne({email: req.body.email}).exec((err, user) => {
@@ -62,8 +63,6 @@ exports.signin = (req, res) => {
 
 
 
-
-
 exports.signout = (req, res) => {
     res.clearCookie("token");
     res.json({message: 'Signout success'});
@@ -72,6 +71,42 @@ exports.signout = (req, res) => {
 
 exports.requireSignin = expressJwt({
     secret: process.env.JWT_SECRET,
-    algorithms: ["HS256"], // added later
-    userProperty: "auth",
+    algorithms: ["HS256"] // added later
+    // userProperty: "auth",
   });
+
+
+
+exports.authMiddleware = (req, res, next) => {
+    const authUserId = req.user._id;
+    User.findById({_id: authUserId}).exec((err, user) => {
+        if(err || !user) {
+            return res.status(400).json({
+                error: 'User not found'
+            });
+        }
+        req.profile = user;
+        next();
+    })
+};
+
+// Admin Check
+exports.adminMiddleware = (req, res, next) => {
+    const adminUserId = req.user._id;
+    User.findById({_id: adminUserId}).exec((err, user) => {
+        if(err || !user) {
+            return res.status(400).json({
+                error: 'User not found'
+            });
+        }
+
+        if(user.role !== 1){
+            return res.status(400).json({
+                error: 'Admin resource. Access denied'
+            });
+        }
+
+        req.profile = user;
+        next();
+    })
+};
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 //bring routes
 const blogRoutes = require('./routes/blog');
 const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
 
 // app
 const app = express();
@@ -31,6 +32,7 @@ if(process.env.NODE_ENV === 'development'){
 // add prefix /api
 app.use('/api', blogRoutes);
 app.use('/api', authRoutes);
+app.use('/api', userRoutes);
 
 
 // routes
