19641a284f752ca95fcde69205bbd05e32cc2bd2
Thanawat <thanawat_bumpengpun@cmu.ac.th>
2021-11-01 17:01:38 +0700

    update: category

--- frontend/components/crud/Category.js
+++ frontend/components/crud/Category.js
@@ -17,17 +17,29 @@ const Category = () => {
     const {name, error, success, categories, removed} = values;
     const token = getCookie('token');
 
+    const clickSubmit = (e) => {
+        e.preventDefault();
+        console.log('create category', name);
+
+    }
+
+    const handleChange = (e) => {
+        setValues({...values, name: e.target.value, error: false, success: false, removed: ''});
+    };
+
     const newCategoryForm = () => (
         <form onSubmit={clickSubmit}>
             <div className="form-group">
                 <label className="text-muted">
                     Name
                 </label>
-                <input type="text" className="form-control" onChange={handlerChange} values={name} required/>
+                <input type="text" className="form-control" onChange={handleChange} values={name} required/>
                 <button type="submit" className="btn btn-dark">Create</button>
             </div>
         </form>
-    )
+    );
+
+    return <>{newCategoryForm}</>;
 };
 
 export default Category;
