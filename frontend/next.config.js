bf49e8ac8f74e58d364e9dc33fda180ba48436bf
Thanawat <thanawat_bumpengpun@cmu.ac.th>
2021-11-01 17:07:29 +0700

    fix: next config module

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,10 @@
-// for import css from modules
-const withCSS = require('@zeit/next-css');
+// // for import css from modules
+// const withCSS = require('@zeit/next-css');
 
-module.exports = withCSS({
+module.exports = {
     publicRuntimeConfig: {
         APP_NAME: '0736b',
         API_DEVELOPMENT: 'http://localhost:8000/api',
         PRODUCTION: false
     }
-});
+};
