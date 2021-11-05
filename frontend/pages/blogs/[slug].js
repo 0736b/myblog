f6feb9d65bc00f427e16e4ba8b8f8f9ff3a057a1
Thanawat <thanawat_bumpengpun@cmu.ac.th>
2021-11-05 16:03:43 +0700

    add: related blog

--- frontend/pages/blogs/[slug].js
+++ frontend/pages/blogs/[slug].js
@@ -1,13 +1,32 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "../../components/Layout";
-import { useState } from "react";
-import { singleBlog } from "../../actions/blog";
+import { useEffect, useState } from "react";
+import { singleBlog, listRelated } from "../../actions/blog";
 import { API, DOMAIN, APP_NAME, FB_APP_ID } from "../../config";
 import renderHTML from "react-render-html";
 import moment from "moment";
+import SmallCard from '../../components/blog/SmallCard';
 
 const SingleBlog = ({ blog, query }) => {
+
+  const [related, setRelated] = useState([]);
+  
+  const loadRelated = () => {
+    listRelated({blog}).then(data => {
+      if(data.error){
+        console.log(data.error)
+      }
+      else {
+        setRelated(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadRelated();
+  }, [])
+
   const head = () => {
     return (
       <Head>
@@ -55,6 +74,16 @@ const SingleBlog = ({ blog, query }) => {
       </Link>
     ));
 
+  const showRelatedBlog = () => {
+    return related.map((blog, i) => (
+      <div key={i} className="col-md-4">
+        <article>
+          <SmallCard blog={blog}/>
+        </article>
+      </div>
+    ))
+  }
+
   return (
     <>
     {head()}
@@ -99,10 +128,10 @@ const SingleBlog = ({ blog, query }) => {
                 )}
               </section>
             </div>
-            <div className="container pb-5">
-              {/* <h4 className="text-center pt-5 pb-5 h2">Related blogs</h4>
-              <hr/> */}
-              <p>show related blogs</p>
+            <div className="container">
+              <h4 className="text-center pt-5 pb-5 h2">Related blogs</h4>
+              <hr/>
+              <div className="row">{showRelatedBlog()}</div>
             </div>
             <div className="container pb-5">
               <p>show comments</p>
