86a11119f2bd47a248fce01838eba775f815f4af
Thanawat <thanawat_bumpengpun@cmu.ac.th>
2021-11-05 16:49:23 +0700

    update: blog update setup

--- frontend/pages/admin/index.js
+++ frontend/pages/admin/index.js
@@ -30,6 +30,12 @@ const AdminIndex = () => {
                   </Link>
                 </li>
 
+                <li className="list-group-item">
+                  <Link href="/admin/crud/blogs">
+                      <a className="text-decoration-none">Update/Delete Blogs</a>
+                  </Link>
+                </li>
+
               </ul>
             </div>
             <div className="col-md-8">right</div>
